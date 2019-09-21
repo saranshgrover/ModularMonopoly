22bc1278ec9932809b98fbe9384fed11661b0161
snackerR <esyoo1204@gmail.com>
2019-09-21 00:05:05 -0400

    add TaxTile.js/.css

---
+++ src/TaxTile.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class TaxTile extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        // Reference all props by doing: this.props.(prop name here). Assume props are accessable 
+        let nameOfProperty = this.props.nameOfProperty;
+        return (
+            <div className="TaxTile">
+                {/* Put html here */}
+                <div className="TaxTileName">
+                    <p>{this.props.propertyName}</p>
+                </div>
+                <div className="TaxTilePicture">
+                    <image src={this.props.imgSrc}></image>
+                </div>
+                <div className="TaxTileFooter">
+                    <p>{this.props.footerPrice}</p>
+                </div>
+            </div>
+        );
+    };
+
+}
