00a806822293c67e16eea8b9cbf42d4917714a8f
saranshgrover <ycubiksrube@gmail.com>
2019-09-21 01:48:06 -0400

    front end testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import HouseTile from "./HouseTile.js";
+import javits from "./images/javits.jpg";
 
 function App() {
+  //let img = require("../public/images/javits.jpg");
   return (
     <div className="App">
       <p>This is a test</p>
+      <HouseTile imgSrc={javits} propertyName="Javits Center" colorGroup="" houseCount={0} hotelCount={0} tileId={1}/>
     </div>
   );
 }
