c306aa8170c57d2c8cf2107d84a6d82b0e04a2a8
Isaiah Galarza <isaiahgala99@gmail.com>
2019-09-21 11:40:43 -0400

    Added redirect when there's no players

--- src/Monopoly.js
+++ src/Monopoly.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import GameBoard from './GameBoard';
+import Player from './Player.js';
+import { Redirect } from 'react-router-dom';
+import InformationBoard from './InformationBoard';
 
 class Monopoly extends Component {
     constructor(props) {
-        super();
-
-        this.state = {
-            userObjects: new Array()
-
-        };
+        super(props);
+        if (typeof this.props.location.state != "undefined")
+            this.state = {
+                readyToPlay: true,
+                userObjects: this.props.location.state.userObjects,
+                currentPlayerTurn: null,
+                turnCount: 0
+            };
+        else
+            this.state = {
+                readyToPlay: false
+            };
     }
+
+    // componentDidMount() {
+    //     if (this.state.userObjects.length == 0) {
+    //         this.setState({readyToPlay: false});
+    //     }
+    // }
 
     render() {
         return (
             <div className="Monopoly">
-                <GameBoard />
+                {!this.state.readyToPlay && <Redirect to='/' /> }
+                <GameBoard userObjects={this.state.userObjects} />
+                <InformationBoard userObjects={this.state.userObjects} />
             </div>
         );
     }
