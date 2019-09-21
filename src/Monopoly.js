32f45cd64e12a8372c391366ef8dc72586e1ac9a
Isaiah Galarza <isaiahgala99@gmail.com>
2019-09-21 01:45:29 -0400

    Changed css to scss and added GameBoard

---
+++ src/Monopoly.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Monopoly.css';
+
+class Monopoly extends Comment {
+    constructor(props) {
+        super();    
+    }
+
+    render() {
+        return (
+            <div className="Monopoly">
+                
+            </div>
+        );
+    }
+}
