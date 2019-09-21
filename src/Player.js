fcfc9633387ca54c79b8b277b9076920844304d6
Isaiah Galarza <isaiahgala99@gmail.com>
2019-09-21 11:41:03 -0400

    Added "hasRolled" value, getters and setters

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,14 @@
 export default class Player {
     constructor() {
         this.balance = 1500;
+        this.currentPosition = 0;
+        this.properties = [];
+        this.isTurn = false;
+        this.isInJail = false;
+        this.getOutOfJailAmount = 0;
+        this.houseCount = 0;
+        this.hotelCount = 0;
+        this.hasRolled = false;
     }
     getName() {
         return this.name;
@@ -14,6 +22,10 @@ export default class Player {
         return this.balance;
     }
 
+    setBalance(amount) {
+        this.balance = amount;
+    }
+
     addToBalance(amount) {
         this.balance += amount;
     }
@@ -24,6 +36,50 @@ export default class Player {
 
     getPieceName() {
         return this.pieceName;
+    }
+
+    getCurrentPosition() {
+        return this.currentPosition;
+    }
+
+    hasRolled() {
+        return this.hasRolled;
+    }
+
+    setRolled(boolean) {
+        this.hasRolled = boolean;
+    }
+
+    isTurn() {
+        return this.isTurn;
+    }
+
+    isInJail() {
+        return this.isInJail;
+    }
+
+    getGetOutOfJailAmount() {
+        return this.getOutOfJailAmount;
+    }
+
+    setGetOutOfJailAmount(amount) {
+        this.getOutOfJailAmount = amount;
+    }
+
+    addToHouseCount(amount) {
+        this.houseCount += amount;
+    }
+
+    getHouseCount() {
+        return this.houseCount;
+    }
+
+    addToHotelCount(amount) {
+        this.hotelCount += amount;
+    }
+
+    getHotelCount() {
+        return this.hotelCount;
     }
 
 }
